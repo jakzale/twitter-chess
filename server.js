e636ea6fa4e2ce5c8f00663502c7fbb3665c08e0
Michal Kawalec <michal@bazzle.me>
2014-02-08 11:33:04 +0000

    a stubby move service

--- server.js
+++ server.js
@@ -1,19 +1,22 @@
 var feathers = require('feathers');
+var http = require('http');
 
-var todoService = {
-    get: function(name, params, callback) {
-        'use strict';
+var move_service = {
+    find: function(params, callback) {
+        callback(null, "heyo");
+    },
 
-        callback(null, {
-            id: name,
-            description: "You have to do " + name + "!"
-        });
+    create: function(data, params, callback) {
+        console.log('adding', data);
+        callback(null, data);
     }
 };
 
 feathers()
     .configure(feathers.socketio())
-    .use('/todo', todoService)
+    .use('/move', move_service)
     .listen(8000);
 
 console.log('Server running on 8000');
+
+
