f0f81b1f6768b9faf77e2dd7afbfba5eab72759e
Jakub Z <jakzale@gmail.com>
2014-02-08 11:28:41 +0000

    Add simple nodemon runner

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,4 +1,27 @@
 module.exports = function(grunt) {
+    'use strict';
     // Simple gruntfile
 
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        nodemon: {
+            dev: {
+                script: 'server.js',
+                options: {
+                    nodeArgs: ['--debug'],
+                    callback: function(nodemon) {
+                        nodemon.on('log', function(event) {
+                            console.log(event.colour);
+                        });
+                    }
+                },
+                ignore: ['node_modules/**'],
+                ext: 'js'
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-nodemon');
+
+    grunt.registerTask('default', ['nodemon']);
 };
