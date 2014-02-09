a5282c049c5f613390f181c2bc5747547e4b6fee
Michal Kawalec <michal@bazzle.me>
2014-02-09 18:27:22 +0000

    contains function is tested

--- test/mocha/chess.js
+++ test/mocha/chess.js
@@ -57,4 +57,21 @@ describe('Chess tests', function() {
             moves.protections.contains([1, 0]).should.be.ok;
         });
     });
+
+    describe('Libs should work', function() {
+        it('contains function works', function() {
+            var test1 = [1, 2, 3];
+
+            test1.contains(1).should.be.ok;
+            test1.contains(2).should.be.ok;
+            test1.contains(3).should.be.ok;
+
+            test1.contains(4).should.not.be.ok;
+
+            var test2 = [['lol', 1], 2];
+            test2.contains(2).should.be.ok;
+            test2.contains(1).should.not.be.ok;
+            test2.contains(['lol', 1]).should.be.ok;
+        });
+    });
 });
